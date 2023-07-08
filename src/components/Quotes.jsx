85897f8f0aa6d160f6ff95ba76ec7ddc4c59ae52
DayTroy <shadowyguy5@gmail.com>
2023-07-08 14:04:36 +0300

    Implemented adding random quote functionality

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import twitterIcon from '../assets/icons/twitter.svg'
 import tumblrIcon from '../assets/icons/tumblr.svg'
 
 const Quotes = () => {
+    const [quote, setQuote] = useState("");
+    const [author, setAuthor] = useState("");
+
+    useEffect(() => {
+        getQuote()
+    }, [])
+
+    const getQuote = () => {
+        let url = `https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            let dataQuotes = data.quotes;
+            let randomNumber = Math.floor(Math.random() * dataQuotes.length);
+            let randomQuote = dataQuotes[randomNumber]
+            setQuote(randomQuote.quote)
+            setAuthor(randomQuote.author)
+        })
+    }
+
+    const handleClick = () => {
+        getQuote()
+    } 
+
     return (
         <div id="quotes-box">
-            <div id="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta eum quod commodi numquam</div>
+            <div id="text">{quote}</div>
             <div className="author">
-                <p>John Doe</p>
+                <p>{author}</p>
             </div>
             <div id="buttons">
                 <div className="social-media">
@@ -18,7 +42,7 @@ const Quotes = () => {
                         <span><img src={tumblrIcon} alt="tumblr" /></span>
                     </a>
                 </div>
-                <button id="new-quote">New Quote</button>
+                <button onClick={handleClick} id="new-quote">New Quote</button>
             </div>
         </div>
     )
