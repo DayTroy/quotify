2754cbb6c63c72f047e4289fa21cef0f1f52493c
DayTroy <shadowyguy5@gmail.com>
2023-07-08 14:54:20 +0300

    Implemented switching theme after clicking on button

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -6,7 +6,11 @@ import axios from "axios";
 const Quotes = () => {
     const [quote, setQuote] = useState("");
     const [author, setAuthor] = useState("");
+    const [color, setColor] = useState();
     const src = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+
+    const colors = ["#191923", "#0E79B2", "#BF1363", "#F39237", "#64B6AC", "#DB5461", "#3B3355", "#1D1E2C", "#9C528B", "#A1CDA8", "#F3A712", "#DB2B39", "#534D41", "#F0CEA0"]
+    const randomColorIndex = Math.floor(Math.random() * colors.length)
 
     useEffect(() => {
         getQuote()
@@ -26,24 +30,26 @@ const Quotes = () => {
 
     const handleClick = () => {
         getQuote()
+        setColor(colors[randomColorIndex])
+        document.body.style.backgroundColor = colors[randomColorIndex] 
     } 
 
     return (
-        <div id="quotes-box">
-            <div id="text">{quote}</div>
-            <div className="author">
+        <div id="quotes-box" >
+            <div style={{color: color}} id="text">{quote}</div>
+            <div style={{color: color}} className="author">
                 <p>{author}</p>
             </div>
             <div id="buttons">
                 <div className="social-media">
-                    <a href="#" id="tweet-quote">
+                    <a href="#" id="tweet-quote" style={{backgroundColor: color}}>
                         <span><img src={twitterIcon} alt="twitter" /></span>
                     </a>
-                    <a href="#" id="tumblr-quote">
-                        <span><img src={tumblrIcon} alt="tumblr" /></span>
+                    <a href="#" id="tumblr-quote" style={{backgroundColor: color}}>
+                        <span><img  src={tumblrIcon} alt="tumblr" /></span>
                     </a>
                 </div>
-                <button onClick={handleClick} id="new-quote">New Quote</button>
+                <button style={{backgroundColor: color}} onClick={handleClick} id="new-quote">New Quote</button>
             </div>
         </div>
     )
