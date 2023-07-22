89fd260797878252e38109cfeb7ebf1cc1908af0
DayTroy <shadowyguy5@gmail.com>
2023-07-23 01:34:30 +0300

    Extracted src from quotes_db.js

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -3,8 +3,11 @@ import axios from "axios";
 import twitterIcon from "../assets/icons/twitter.svg";
 import tumblrIcon from "../assets/icons/tumblr.svg";
 import colors from "../utils/colors";
-import src from "../utils/quotes_db";
 import "./Quotes.css"
+
+const src =
+    "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+
 
 const Quotes = () => {
   const [quote, setQuote] = useState("");
